fb1cae74874c7518dbea7bfaa5c8d968ad9d2785
Maritantantan <m.cantillana@duocuc.cl>
2025-12-08 23:05:26 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -35,7 +35,7 @@ api.interceptors.response.use(
             localStorage.removeItem('token');
             localStorage.removeItem('user');
             if (window.location.pathname.includes('/admin')) {
-                window.location.href = '/';
+                window.location.href = '/home';
             }
         }
         return Promise.reject(error);
